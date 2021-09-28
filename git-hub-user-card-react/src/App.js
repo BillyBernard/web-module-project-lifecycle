ba74584b62871d1ee8c32baa0082bae6fd6ac71e
BillyBernard <billybernard54@gmail.com>
2021-09-28 13:13:16 -0600

    got info displayed for users and followers

--- git-hub-user-card-react/src/App.js
+++ git-hub-user-card-react/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 class App extends React.Component {
   state = {
     userData: [],
+    followers: [],
   };
 
   componentDidMount() {
@@ -15,6 +16,14 @@ class App extends React.Component {
         userData: resp.data,
       });
     });
+    axios.get('https://api.github.com/users/BillyBernard/followers')
+    .then((resp) => {
+      console.log(resp.data);
+      this.setState({
+        ...this.State,
+        followers: resp.data,
+      });
+    });
   }
 
   render() {
@@ -22,8 +31,21 @@ class App extends React.Component {
       <div className='App'>
         <h1>Github User Card</h1>
         <div>
-          {this.state.userData.map((item) => {
-            return <p>{item.name}</p>
+          <img src={this.state.userData.avatar_url}/> 
+          <p>Name: {this.state.userData.name}</p>
+          <p>Login: {this.state.userData.login}</p>
+          <p>Email: {this.state.userData.email}</p>
+        </div>
+        <div>
+          <h2>Followers: {this.state.userData.followers}</h2>
+          {this.state.followers.map((item) => {
+            return (
+            <div key={item.id}>
+              <div><img src={item.avatar_url}/></div>
+              <p>{item.login}</p>
+              <p>{item.type}</p>
+            </div>
+            )
           })}
         </div>
       </div>
