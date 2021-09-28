170982b5c7bef90c9d4f78cdf0391a5242f38d33
BillyBernard <billybernard54@gmail.com>
2021-09-28 13:56:17 -0600

    styled the card

--- git-hub-user-card-react/src/App.js
+++ git-hub-user-card-react/src/App.js
@@ -1,5 +1,44 @@
 import React from 'react';
 import axios from 'axios';
+import styled from "styled-components";
+
+const AppBack = styled.div`
+  background-color: blue;
+`
+
+const MyCard = styled.div`
+  background-color: orange;
+  padding-bottom: 2em;
+`
+const FollowerCard = styled.div`
+  background-color: blue;
+  padding-top: 2em;
+`
+const UserImg = styled.img`
+  border-radius: 50%
+`
+
+const CardItems = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  color: white;
+`
+
+const CardInfo = styled.div`
+  background-color: grey;
+  padding: 2em;
+  margin: 2em;
+  border-radius: 5px;
+`
+
+const StyledH2 = styled.h2`
+  color: white;
+  background-color: blue;
+  text-align: center;
+  padding: 2em;
+`
 
 class App extends React.Component {
   state = {
@@ -28,27 +67,42 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className='App'>
-        <h1>Github User Card</h1>
+      <AppBack className='App'>
+        
+        <MyCard>
+          <CardItems>
+          <h1>Github User Card</h1>
+          <UserImg src={this.state.userData.avatar_url} alt="me"/>
+          <CardInfo> 
+            <p>Name: {this.state.userData.name}</p>
+            <p>Login: {this.state.userData.login}</p>
+            <p>Email: {this.state.userData.email}</p>
+            <p>Followers: {this.state.userData.followers}</p>
+            <p>Following: {this.state.userData.following}</p>
+            
+            </CardInfo>
+          </CardItems>
+        </MyCard>
+        
         <div>
-          <img src={this.state.userData.avatar_url}/> 
-          <p>Name: {this.state.userData.name}</p>
-          <p>Login: {this.state.userData.login}</p>
-          <p>Email: {this.state.userData.email}</p>
-        </div>
-        <div>
-          <h2>Followers: {this.state.userData.followers}</h2>
+          <StyledH2>My Followers: </StyledH2>
           {this.state.followers.map((item) => {
             return (
-            <div key={item.id}>
-              <div><img src={item.avatar_url}/></div>
-              <p>{item.login}</p>
-              <p>{item.type}</p>
-            </div>
+              
+            <FollowerCard key={item.id}>
+              
+              <CardItems>
+                <UserImg src={item.avatar_url} alt="followers"/>
+                <CardInfo>
+                  <p>Login: {item.login}</p>
+                  <p>Type: {item.type}</p>
+                </CardInfo>
+              </CardItems>
+            </FollowerCard>
             )
           })}
         </div>
-      </div>
+      </AppBack>
     )
   }
 
